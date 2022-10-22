5cf4d9142e83453c4b5142966a2222ec04e070d9
muzammilshaikh12 <110051885+muzammilshaikh12@users.noreply.github.com>
2022-10-22 15:57:48 +0530

    Making it more dynamic

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import ExpenseForm from './ExpenseForm'
 
 import './NewExpense.css'
 
 const NewExpense = (props) =>  {
+    const[isNotEditing,setEditing] = useState(false)
     const expenseDataHandler = (NewExpenseData) => {
        const newData = {
         ...NewExpenseData
@@ -12,10 +13,20 @@ const NewExpense = (props) =>  {
 
     //    console.log(newData)
        props.onSaveFinalData(newData)
+       setEditing(false)
+    }
+
+    const EditHandler = () => {
+        setEditing(true)
+    }
+
+    const StopEditing = () => {
+        setEditing(false)
     }
 
     return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={expenseDataHandler}/>
+       {!isNotEditing && <button onClick={EditHandler}>Add Expense</button>}
+       {isNotEditing && <ExpenseForm onSaveExpenseData={expenseDataHandler} onCancel={StopEditing}/>}
     </div>
 }
 
